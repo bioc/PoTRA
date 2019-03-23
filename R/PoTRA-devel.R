9b1bf6d04c13d7b6693bbab847b52302cfbe2501
PoTRA-Package <48025742+PoTRA-Package@users.noreply.github.com>
2019-03-23 12:57:06 -0400

    Update PoTRA-devel

--- R/PoTRA-devel.R
+++ R/PoTRA-devel.R
@@ -1,79 +1,79 @@
 PoTRA.corN <- function(mydata,genelist,Num.sample.normal,Num.sample.case,Pathway.database, PR.quantile) {
     
-    Fishertest<-c()
-    TheNumOfHubGene.normal<-c()
-    TheNumOfHubGene.case<-c()
-    E.normal<-c()
-    E.case<-c()
-    length.pathway<-c()
-    kstest<-c()
-    pathwaynames <- c()
+Fishertest<-c()
+TheNumOfHubGene.normal<-c()
+TheNumOfHubGene.case<-c()
+E.normal<-c()
+E.case<-c()
+length.pathway<-c()
+kstest<-c()
+pathwaynames <- c()
     
-    #humanPharmGKB <- pathways("hsapiens", "pharmgkb")
-    #humanBiocarta <- pathways("hsapiens", "biocarta")
-    #humanKEGG <- pathways("hsapiens", "kegg")
+#humanPharmGKB <- pathways("hsapiens", "pharmgkb")
+#humanBiocarta <- pathways("hsapiens", "biocarta")
+#humanKEGG <- pathways("hsapiens", "kegg")
     
-	for (x in seq_along(Pathway.database[seq_along(Pathway.database)])){       
-        print(x)
-        p0 <-Pathway.database[[x]]
-        pathwaynames[x] <- p0@title
-        p <- convertIdentifiers(p0, "entrez")
-        g<-pathwayGraph(p) 
-        nodelist<-nodes(g)
-        graph.path<-igraph.from.graphNEL(g)    
-              
-        
-        genelist_reformatted <- sprintf('ENTREZID:%s', genelist) 
+for (x in seq_along(Pathway.database[seq_along(Pathway.database)])){       
+    
+    print(x)
+    
+    p0 <-Pathway.database[[x]]
+    pathwaynames[x] <- p0@title
+    p <- convertIdentifiers(p0, "entrez")
+    g<-pathwayGraph(p) 
+    nodelist<-nodes(g)
+    graph.path<-igraph.from.graphNEL(g)          
+    genelist_reformatted <- sprintf('ENTREZID:%s', genelist) 
 		
-        length.intersect<-length(intersect(unlist(nodelist),
-			unlist(genelist_reformatted)))
+    length.intersect<-length(intersect(unlist(nodelist),
+        unlist(genelist_reformatted)))
 			
-        length.pathway[x]<-length.intersect
-        graph.path<-induced_subgraph(graph.path, 
-			as.character(intersect(unlist(nodelist),
-				unlist(genelist_reformatted))))
+    length.pathway[x]<-length.intersect
+    graph.path<-induced_subgraph(graph.path, 
+        as.character(intersect(unlist(nodelist),
+            unlist(genelist_reformatted))))
       
-        
-        if (length.intersect<5){
-            next
-        }else{
+    if (length.intersect<5){
+        next
+        }
+	else{
 		
         #########################################################    
         #collect expression data of genes for a specific pathway# 
 	#across normal and tumor samples.                       #  
         #########################################################    
-            path<-data.frame(matrix(0,
-				length.intersect,
-					(Num.sample.normal+Num.sample.case)))
+            sum_samples = Num.sample.normal+Num.sample.case 
+            path<-data.frame(matrix(0, length.intersect, sum_samples))
 					
             a<- c()
             
             for (j in seq_len(length.intersect)){
-            
-				a[j]<-intersect(unlist(nodelist),unlist(genelist_reformatted))[j]  
-				
-		    		#################################################
-				#collect expression data of genes for a specific# 
-		    		#pathway across normal and tumor samples.       #
-                                #################################################
-				path[j,]<-mydata[which(genelist_reformatted==a[j]),]  
+                a[j]<-intersect(unlist(nodelist),unlist(genelist_reformatted))[j]  
+		#################################################
+		#collect expression data of genes for a specific# 
+		#pathway across normal and tumor samples.       #
+                #################################################
+		path[j,]<-mydata[which(genelist_reformatted==a[j]),]  
             }
             
 	    ################################################################
             #Construct a gene-gene network for normal samples and calculate# 
 	    #PageRank values for each gene in this network.	           #
             ################################################################ 
-            cor.normal <- apply(path[,seq_len(Num.sample.normal)],1, function(x) {
-					apply(path[,seq_len(Num.sample.normal)], 1, function(y) {
-						cor.test(x,y)[[3]] 
-						})
-						})
+            len_samp_norm = seq_len(Num.sample.normal)
+	   
+	    cor.normal_1<-apply(path[,len_samp_norm], 1, function(y) {
+	        cor.test(x,y)[[3]] 
+            })
+
+	    cor.normal<-apply(path[,len_samp_norm],1, function(x) {
+	        cor.normal_1
+	    })
 
             cor.normal<-as.matrix(cor.normal) 
             
-            cor.normal.adj<-matrix(p.adjust(cor.normal,method="fdr"),
-				length.intersect,
-					length.intersect)
+	    stat_adj_norm = p.adjust(cor.normal,method="fdr")
+            cor.normal.adj<-matrix(stat_adj_norm,length.intersect,length.intersect)
             
             cor.normal.adj[ cor.normal.adj > 0.05 ] <- 0
             cor.normal.adj[ is.na(cor.normal.adj)] <- 0
@@ -87,17 +87,22 @@ PoTRA.corN <- function(mydata,genelist,Num.sample.normal,Num.sample.case,Pathway
             #Construct a gene-gene network for tumor samples and calculate# 
             #PageRank values for each gene in this network.		  #
             ###############################################################
-	    range=(Num.sample.normal+1):(Num.sample.normal+Num.sample.case)
-            cor.case <- apply(path[,range], 1, function(x) {
-				apply(path[,range], 1, function(y) {
-					cor.test(x,y)[[3]] 
-					})
-					})
+	    range_start = Num.sample.normal+1
+	    range_end = Num.sample.normal+Num.sample.case
+	    range = range_start:range_end
+            
+	    cor.case_1<-apply(path[,range], 1, function(y) {
+                cor.test(x,y)[[3]] 
+	    })	
+	    
+	    cor.case <- apply(path[,range], 1, function(x) {
+		cor.case_1
+            })
             
             cor.case<-as.matrix(cor.case) 
-            cor.case.adj<-matrix(p.adjust(cor.case,method="fdr"),
-				length.intersect,
-					length.intersect)
+		
+	    stat_adj_case = p.adjust(cor.case,method="fdr")
+	    cor.case.adj<-matrix(stat_adj_case,length.intersect,length.intersect)
             
             cor.case.adj[ cor.case.adj > 0.05 ] <- 0
             cor.case.adj[ is.na(cor.case.adj)] <- 0
@@ -155,13 +160,12 @@ PoTRA.corN <- function(mydata,genelist,Num.sample.normal,Num.sample.case,Pathway
             
         }
     }
-    return(list(Fishertest.p.value=Fishertest,
-	KStest.p.value=kstest,
-	LengthOfPathway=length.pathway,
-	TheNumberOfHubGenes.normal=TheNumOfHubGene.normal,
-	TheNumberOfHubGenes.case=TheNumOfHubGene.case,
-	TheNumberOfEdges.normal=E.normal,
-	TheNumberOfEdges.case=E.case,
-	PathwayName=pathwaynames))
-	
+return(list(Fishertest.p.value=Fishertest,
+    KStest.p.value=kstest,
+    LengthOfPathway=length.pathway,
+    TheNumberOfHubGenes.normal=TheNumOfHubGene.normal,
+    TheNumberOfHubGenes.case=TheNumOfHubGene.case,
+    TheNumberOfEdges.normal=E.normal,
+    TheNumberOfEdges.case=E.case,
+    PathwayName=pathwaynames))
 }
