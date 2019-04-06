32e8a6931ac8cd8bfa448c53fb26053cffe3fbb5
PoTRA-Package <48025742+PoTRA-Package@users.noreply.github.com>
2019-04-06 10:19:06 -0400

    Update PoTRA.R

--- R/PoTRA.R
+++ R/PoTRA.R
@@ -9,17 +9,26 @@
 #'                       Num.sample.case=Num.sample.case,
 #'                       Pathway.database=Pathway.database,
 #'                       PR.quantile=PR.quantile)
+#
+#'\subsection{Object Definitions for PoTRA.corN}
+#' \describe{ 
+#'  \item{mydata}{Dataframe that contains rownames consisting of entrez gene identifiers and columns representing normals and samples from gene expression data.}
+#'  \item{genelist}{Dataframe that consists of a single column of entrez gene identifiers (the same as those found in the rownames(mydata)).}
+#'  \item{Num.sample.normal}{Represents normal samples in the mydata dataframe.}
+#'  \item{Num.sample.case}{Represents case samples in the mydata dataframe.}
+#'  \item{Pathway.database}{Object contains gene lists from KEGG, Biocarta and PharmGKB databases, and these are made available through the devel graphite library.}
+#'  \item{PR.quantile}{Object contains the percentile of PageRank scores as a cutoff for hub genes, the recommended percentile is 0.95.}
+#'}
+#'\subsection{Example Definitions for Pathway.database}
 #'
-#'Pathway.database (options):
-#'humanKEGG = pathways('hsapiens','kegg'),
-#'humanBiocarta = pathways('hsapiens', 'biocarta'),
-#'humanPharmGKB = pathways('hsapiens', 'pharmgkb').
+#' Pathway.database = humanKEGG, etc (see below)
 #'
-#'mydata is a dataframe that contains rownames consisting of entrez gene identifiers and columns representing normals and samples from gene expression data.
-#'The object genelist is a dataframe that consists of a single column of entrez gene identifiers (the same as those found in the rownames(mydata)).
-#'Other objects such as Num.sample.normal and Num.sample.case represent normals and cases in the mydata dataframe. The Pathway.database object contains gene
-#'lists from KEGG, Biocarta and PharmGKB databases, and these are made available through the graphite library.
-#'The PR.quantile object contains the percentile of PageRank scores as a cutoff for hub genes, the recommended percentile is 0.95.
+#' \describe{
+#'  \item{KEGG}{humanKEGG = pathways('hsapiens','kegg')}
+#'  \item{Reactome}{humanReactome = pathways('hsapiens','reactome')}
+#'  \item{Biocarta}{humanBiocarta = pathways('hsapiens', 'biocarta')}
+#'  \item{PharmGKB}{humanPharmGKB = pathways('hsapiens', 'pharmgkb')}
+#'}
 #' @docType package
 #' @author Chaoxing Li, Li Liu, Valentine Dinu \email{valentine.dinu@asu.edu}
 #'
